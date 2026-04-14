b2f9306324604f6a590241a905c58483d7e1f5ca
Samuel-Valentini <samuelvitalia@gmail.com>
2026-04-14 16:20:19 +0200

    Initial commit

---
+++ src/main/java/samuelvalentini/u5d7ex/U5d7exApplication.java
@@ -0,0 +1,13 @@
+package samuelvalentini.u5d7ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d7exApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d7exApplication.class, args);
+	}
+
+}
---
+++ src/test/java/samuelvalentini/u5d7ex/U5d7exApplicationTests.java
@@ -0,0 +1,13 @@
+package samuelvalentini.u5d7ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d7exApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
